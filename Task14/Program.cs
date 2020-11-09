118b35ae0a918e98b3509ae36ef98f63364ecbc6
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-11-09 03:37:15 +0300

    Добавьте файлы проекта.

---
+++ Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter email, url or file path:");
+            string s = Console.ReadLine();
+            bool x = Validating.Validate(s);
+            if (!x)
+                Console.WriteLine("wrong input!");
+            else
+                Console.WriteLine("all right");
+        }
+    }
+}
---
+++ Task14/Validating.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task14
+{
+    static class Validating
+    {
+        public static bool Validate(string s)
+        {
+            Match match = Regex.Match(s, 
+                @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            if (match.Success)
+                return true;
+            match = Regex.Match(s, @"((mailto\:|(news|(ht|f)tp(s?))\://){1}\S+)");
+            if (match.Success)
+                return true;
+            match = Regex.Match(s, @"([a-zA-Z]:(\\w+)*\\[a-zA-Z0_9]+)");
+            if (match.Success)
+                return true;
+            return false;
+        }
+
+    }
+}
